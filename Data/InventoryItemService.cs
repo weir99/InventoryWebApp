cae0dd4a41b959318b2a2d15b04531049f3fa24e
weir99 <10331058+weir99@users.noreply.github.com>
2022-05-18 17:56:49 -0500

    Made IDs auto generated

--- Data/InventoryItemService.cs
+++ Data/InventoryItemService.cs
@@ -7,8 +7,11 @@ namespace InventoryWebApp.Data;
 
 public class InventoryItemService
 {
-    string dataDocPath = Path.Combine(Directory.GetCurrentDirectory(), "Data", "Inventory.csv");
+    private string dataDocPath = Path.Combine(Directory.GetCurrentDirectory(), "Data", "Inventory.csv");
+    private int recordCount; //Tracks number of records for creating IDs
 
+    //Gets next ID to be added
+    public int NextId() => recordCount + 1;
     public async Task<List<InventoryItem>> GetInventoryAsync()
     {
         using (var reader = new StreamReader(dataDocPath))
@@ -17,6 +20,7 @@ public class InventoryItemService
             IAsyncEnumerable<InventoryItem> records = csv.GetRecordsAsync<InventoryItem>();
             await foreach (var record in records){
                 inventory.Add(record);
+                ++recordCount;
             }
             return inventory;
         }
@@ -30,13 +34,14 @@ public class InventoryItemService
         var config = new CsvConfiguration(System.Globalization.CultureInfo.InvariantCulture){
             HasHeaderRecord = false
         };
+        item.Id = recordCount; // Set item ID
+        inventory.Add(item);
         using (var stream = File.Open(dataDocPath, FileMode.Append))
         using (var writer = new StreamWriter(stream))
         using (var csv = new CsvWriter(writer, config)){
             csv.NextRecord();
             csv.WriteRecord(item);
         }
-        inventory.Add(item);
         return inventory;
     }
 }
