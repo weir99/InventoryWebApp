0a1eb7cb8619724be98b8fd179462c564bce9bcf
weir99 <10331058+weir99@users.noreply.github.com>
2022-05-18 20:08:15 -0500

    Added update functionality

--- Data/InventoryItemService.cs
+++ Data/InventoryItemService.cs
@@ -1,6 +1,6 @@
 using CsvHelper.Configuration;
 using CsvHelper;
-using CsvHelper.Expressions;
+using Microsoft.AspNetCore.Mvc;
 namespace InventoryWebApp.Data;
 
 
@@ -8,6 +8,7 @@ namespace InventoryWebApp.Data;
 public class InventoryItemService
 {
     private string dataDocPath = Path.Combine(Directory.GetCurrentDirectory(), "Data", "Inventory.csv");
+    private string tempDocPath = Path.Combine(Directory.GetCurrentDirectory(), "Data", "tmp_Inventory.csv");
     private int recordCount; //Tracks number of records for creating IDs
 
     //Gets next ID to be added
@@ -57,8 +58,45 @@ public class InventoryItemService
 
     // Update existing item, compare IDs to determine what to change
     // Compare oldItem, to expected item based off ID to ensure valid
-    // 
+    // Copies data to temp file, then saves that temp file as original
     public async Task<List<InventoryItem>> UpdateAsync(InventoryItem oldItem, InventoryItem newItem){
+        int targetId = oldItem.Id;
+        bool changed = false;
+        newItem.Id = targetId;
+        using (var writer = new StreamWriter(tempDocPath))
+        using (var csvW = new CsvWriter(writer, System.Globalization.CultureInfo.InvariantCulture))
+        using (var reader = new StreamReader(dataDocPath))
+        using (var csvR = new CsvReader(reader, System.Globalization.CultureInfo.InvariantCulture)){
+            IAsyncEnumerable<InventoryItem> records = csvR.GetRecordsAsync<InventoryItem>();
+            csvW.WriteHeader<InventoryItem>();
+            csvW.NextRecord();
+            await foreach (var record in records){
+                if(record.Id == targetId){
+                     // Check that we are updating the right thing
+                    if(record.Name != oldItem.Name || record.Quantity != oldItem.Quantity){
+                        throw new ArgumentException("Object to change doesn't match given", nameof(oldItem));
+                    } 
+                    else{
+                        Console.WriteLine("updating");
+                        csvW.WriteRecord(newItem);
+                        csvW.NextRecord();
+                    }
+                    changed = true;
+                }
+                else{
+                    csvW.WriteRecord(record);
+                    csvW.NextRecord();
+                }
+                Console.WriteLine(record.Id);
+                Console.WriteLine(record.Name);
+                Console.WriteLine(record.Quantity);
+            }
+        }
+        // Check that we are updating something, and the write something
+        if(!changed) throw new ArgumentOutOfRangeException("Cannot find item to update", nameof(oldItem));
+        File.Delete(dataDocPath);
+        File.Copy(tempDocPath, dataDocPath);
+        File.Delete(tempDocPath);
         return await GetInventoryAsync();
     }
 }
